736d5d579a5519833122922db783c5e266135d2e
Juyoung Kim <juyoungk23@gmail.com>
2022-05-25 18:58:29 -0400

    todo list

--- cryptoMACDBot/Program.cs
+++ cryptoMACDBot/Program.cs
@@ -7,9 +7,14 @@ namespace cryptoMACDBot
 {
     class Program
     {
+
+        // Todo:
+        // 1. Figure out how to update the MACD indicator as the data is streaming live
+
+
         static async Task Main(string[] args)
         {
-
+            
             string apiKey = "07661b27d60a86197091a49404aff506";
             string apiSecret = "uEthX/aoXnXu8y9BP6r9Bj6iMcs6of0K45JIDn3fKi2rGqRNXnkeLQmqjOdFLEU8fqR4CeKGr2Ssff76TPQFog==";
             string passphrase = "sysr3fz9sbb";
